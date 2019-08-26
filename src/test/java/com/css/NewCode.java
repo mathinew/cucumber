9026abbf5bd82513543a718ab13d804912d86166
mathinew <mathi.svb@gmail.com>
2019-08-26 22:45:25 +0530

    new changes

--- src/test/java/com/css/NewCode.java
+++ src/test/java/com/css/NewCode.java
@@ -18,7 +18,7 @@ import cucumber.api.java.en.When;
 public class NewCode {
 	
 	static WebDriver driver;
-	//WebDriverWait w = new WebDriverWait(driver, 100);
+	
 	
 	@Given("Use should launch browser and url")
 	public void use_should_launch_browser_and_url() throws InterruptedException {
@@ -26,32 +26,48 @@ public class NewCode {
 		System.setProperty("webdriver.chrome.driver", "D:\\workspace\\SelTstng\\drivers\\chromedriver.exe");
 		 driver = new ChromeDriver();
 		driver.get("http://www.demo.guru99.com/telecom/");
-		//driver.manage().window().maximize();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(15000, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();
+		//driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();
 
 	}
 
 	@Given("User click Add customer")
 	public void user_click_Add_customer()  {
 		
-		//driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();
-		
+		driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();
+		//WebDriverWait w = new WebDriverWait(driver, 100);
 		//w.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[text()='Add Customer'])[1]"))).click();
 
 	}
 
-	@When("User provide valid details")
-	public void user_provide_valid_details() {
-		//driver.manage().timeouts().implicitlyWait(15000, TimeUnit.SECONDS);
+//	@When("User provide valid details")
+//	public void user_provide_valid_details() {
+//		//driver.manage().timeouts().implicitlyWait(15000, TimeUnit.SECONDS);
+//		driver.findElement(By.xpath("//label[text()='Done']")).click();
+//		driver.findElement(By.id("fname")).sendKeys("mathi");
+//		driver.findElement(By.id("lname")).sendKeys("arasi");
+//		driver.findElement(By.id("email")).sendKeys("mathi.svb@gmail.com");
+//		driver.findElement(By.xpath("//textarea[@name ='addr']")).sendKeys("chennai");
+//		driver.findElement(By.id("telephoneno")).sendKeys("1243433433");
+//		driver.findElement(By.xpath("//input[@name='submit']")).click();
+//	}
+	
+	@When("User provide valid details {string}, {string}, {string} , {string}, {string}")
+	public void user_provide_valid_details(String fname, String lname, String email, String address, String phone) throws InterruptedException {
+
+		driver.manage().timeouts().implicitlyWait(15000, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//label[text()='Done']")).click();
-		driver.findElement(By.id("fname")).sendKeys("mathi");
-		driver.findElement(By.id("lname")).sendKeys("arasi");
-		driver.findElement(By.id("email")).sendKeys("mathi.svb@gmail.com");
-		driver.findElement(By.xpath("//textarea[@name ='addr']")).sendKeys("chennai");
-		driver.findElement(By.id("telephoneno")).sendKeys("1243433433");
+		driver.findElement(By.id("fname")).sendKeys(fname);
+		driver.findElement(By.id("lname")).sendKeys(lname);
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.xpath("//textarea[@name ='addr']")).sendKeys(address);
+		driver.findElement(By.id("telephoneno")).sendKeys(phone);
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[@name='submit']")).click();
+		
 	}
+	
 
 	@Then("Verify customer ID generated for entered details")
 	public void verify_customer_ID_generated_for_entered_details() {
@@ -60,7 +76,5 @@ public class NewCode {
 		
 	}
 
-
-	
 
 }
---
+++ src/test/resources/feature/Outline.feature
@@ -0,0 +1,21 @@
+
+Feature: Adding Customer ID
+
+
+  Scenario Outline: Generate Customer ID
+    Given Use should launch browser and url
+    And User click Add customer
+    When User provide valid details "<fname>", "<lname>", "<email>" , "<address>", "<phone>"
+    Then Verify customer ID generated for entered details
+
+
+
+    Examples: 
+      | fname  | lname | email  |address| phone | 
+      | fnamea | lnamea | test1@gmail.com | chennai |367895678|
+			| fnameb | lnameb | test2@gmail.com  |madurai |567676756|
+			| fnamec | lnamec | test3@gmail.com |bangalore| 367895678|
+			| fnamec | lnamed | test4@gmail.com | trichy|362345478|
+			
+			
+			
